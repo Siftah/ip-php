d01f7235342cff8bd69d457196023500fca5d658
John Baxendale <John@siftah.com>
2022-07-06 13:45:20 +0200

    Update index.php
    Replaced with echo'ing of IP

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
+<html><head><title>Current IP Check</title></head><body>Current IP Address: 
 <?php
-require __DIR__ . '/vendor/autoload.php';
-
-use Cowsayphp\Farm;
-
-header('Content-Type: text/plain');
-
-$text = "Set a message by adding ?message=<message here> to the URL";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
-
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+echo $_SERVER['REMOTE_ADDR'];
+?>
+</body></html>
